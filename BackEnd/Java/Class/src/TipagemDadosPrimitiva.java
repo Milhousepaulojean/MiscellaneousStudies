9a10290f4261f831f8a99a0d7c23d8ce4ea1cdfe
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2020-11-24 22:53:06 -0300

    Java: Ajustes

---
+++ BackEnd/Java/Class/src/TipagemDadosPrimitiva.java
@@ -0,0 +1,31 @@
+public class TipagemDadosPrimitiva {
+    public static void main(String[] args) {
+        ///Boolean
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        //Number
+        int valorInteiro = 50;
+
+        //Long
+        Long valorLong = 50l;
+
+        //Float
+        float pontoflutuante = 5.000000f;
+
+        //Double
+        double doub = 5.5;
+
+        //Character
+        char c = 't';
+
+        System.out.println("o valor e: "+verdadeiro);
+        System.out.println("o valor e: "+ falso);
+        System.out.println("O Valor inteiro informado foi: "+valorInteiro);
+        System.out.println("O Valor Long informado foi: "+ valorLong);
+        System.out.println("O Valor Float informado foi: "+ pontoflutuante);
+        System.out.println("O Valor Double informado foi: "+ doub);
+        System.out.println("O Valor Character informado foi: "+ c);
+
+    }
+}
