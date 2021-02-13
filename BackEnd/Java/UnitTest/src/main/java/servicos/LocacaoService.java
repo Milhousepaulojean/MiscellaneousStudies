2c600cabcab9d89fc7d7d99f8b6a445a4d8dd8ca
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2021-02-13 00:19:59 -0300

    Commit aplicado com o conceito de TestUnitario utilizando conceitos first e 3A

---
+++ BackEnd/Java/UnitTest/src/main/java/servicos/LocacaoService.java
@@ -0,0 +1,47 @@
+package servicos;
+
+import entidades.DataUtils;
+import utils.Filme;
+import utils.Locacao;
+import utils.Usuario;
+import static entidades.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		//Cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1",15 , 2.1);
+
+		//Acao
+		Locacao locacao  =  service.alugarFilme(usuario, filme);
+
+		//Verificacao
+		System.out.println(locacao.getValor() == 2.1);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao() , new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno() , DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
