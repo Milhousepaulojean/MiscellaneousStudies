1b8199039aa294bba285709ca1da958e72eda966
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2021-02-14 19:40:02 -0300

    Commit aplicado com demonstracao do uso dos Asserts.

--- Java/UnitTest/UnitTest/src/test/java/servicos/LocacaoServiceTest.java
+++ Java/UnitTest/UnitTest/src/test/java/servicos/LocacaoServiceTest.java
@@ -24,9 +24,42 @@ class LocacaoServiceTest {
         Locacao locacao  =  service.alugarFilme(usuario, filme);
 
         //Verificacao
-        Assert.assertTrue(locacao.getValor() == 2.1);
+        Assert.assertEquals(2.1, locacao.getValor() , 0.01);
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao() , new Date()));
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno() , DataUtils.obterDataComDiferencaDias(1)));
+    }
+
+    @Test
+    void asserts(){
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+
+        Assert.assertEquals("Erro de comparacao", 1, 1);
+        Assert.assertEquals(0.51234, 0.512, 0.001);
+        Assert.assertEquals(Math.PI, 3.14, 0.01);
+
+        int i = 5;
+        Integer i2 = 5;
+        Assert.assertEquals(Integer.valueOf(i), i2);
+        Assert.assertEquals(i, i2.intValue());
+
+        Assert.assertEquals("bola", "bola");
+        Assert.assertNotEquals("bola", "casa");
+        Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+        Assert.assertTrue("bola".startsWith("bo"));
+
+        Usuario u1 = new Usuario("Usuario 1");
+        Usuario u2 = new Usuario("Usuario 1");
+        Usuario u3 = null;
+
+        Assert.assertEquals(u1, u2);
+
+        Assert.assertSame(u2, u2);
+        Assert.assertNotSame(u1, u2);
+
+        Assert.assertNull(u3);
+        Assert.assertNotNull(u2);
+
     }
 }
 
