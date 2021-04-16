17b644bb40b9c6fbf45fe81112d276c9573d21f8
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2021-04-15 23:24:45 -0300

    nodejs: Commit aplicado para estudo de conexao .

--- 08.BancodeDados/server.js
+++ 08.BancodeDados/server.js
@@ -1,4 +1,15 @@
+<<<<<<< HEAD
 var conn = require("./config")();
+=======
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'case_study01'
+})
+>>>>>>> b1c55f5 (nodejs: Commit aplicado para estudo de conexao .)
 
 conn.query('select * from Persons;', function(error, result){
     console.log(result)
