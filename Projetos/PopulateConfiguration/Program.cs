b7f2f9d51339fb2756b085f59c34d0b6221aa88b
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2021-05-03 19:32:20 -0300

    dotNet: Commit aplicado com exemplo para popular configuration in Memoria com no.

---
+++ Projetos/PopulateConfiguration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace PopulateConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"Key1", "Value1"},
+                {"Nested:Key1", "NestedValue1"},
+                {"Nested:Key2", "NestedValue2"}
+            };
+
+            var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(myConfiguration)
+            .Build();
+
+            Console.WriteLine(configuration.GetSection("Key1").Value);
+            Console.WriteLine(configuration.GetSection("Nested").GetSection("Key1").Value);
+            Console.WriteLine(configuration.GetSection("Nested").GetSection("key2").Value);
+        }
+    }
+}
