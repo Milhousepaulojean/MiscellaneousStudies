435e7cdb759f3fe09bfa0bb00663e5693da223a7
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2021-05-23 17:41:54 -0300

    nodejs: Commit aplicado com a leitura de itens dentro da pastas

---
+++ EstudosBasicos/26.LeituradePastas/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+require('dotenv').config()
+var qtd = 0; 
+
+
+
+fs.readdir(process.env.PATH_FOLDER, (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+        qtd++;
+    });
+    console.log("Quantidade de itens: ",qtd)
+});
