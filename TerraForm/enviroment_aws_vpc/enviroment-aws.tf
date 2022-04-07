b7d41187c5f59d29608604c4634387c7a5745169
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2022-04-06 22:07:22 -0300

    Este commit aplicado cria ambientes uma vpc com subnet

---
+++ TerraForm/enviroment_aws_vpc/enviroment-aws.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "new-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "name" = "vpc-1"
+  }
+}
+
+data "aws_availability_zones" "zone" {}
+
+# output "az" {
+#   value = "${data.aws_availability_zones.zone.names}"
+# }
+
+
+resource "aws_subnet" "aws-subnet-1" {
+  availability_zone = "sa-east-1a"
+  vpc_id = aws_vpc.new-vpc.id
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    "name" = "subnet1"
+  }
+}
