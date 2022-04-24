c86ec8807cb695d581e676ee01177b5c544e9a37
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2022-04-24 18:18:59 -0300

    Este commit aplicado ajusta aplicacao para funcionamento com criacao de rotas

--- TypeScript/CriandoProjeto/src/shared/http/server.ts
+++ TypeScript/CriandoProjeto/src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('server up');
+});
