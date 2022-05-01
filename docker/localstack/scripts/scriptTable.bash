7036167e9fe4e9db00a03e76ec91f08796b61c43
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2022-05-01 20:22:50 -0300

    Este commit aplicado aplica o uso do docker compes

---
+++ docker/localstack/scripts/scriptTable.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "########### Creating table with global secondary index ###########"
+        aws dynamodb --endpoint-url=http://localhost:4566 create-table \
+            --table-name Music \
+            --attribute-definitions \
+                AttributeName=Artist,AttributeType=S \
+                AttributeName=SongTitle,AttributeType=S \
+            --key-schema \
+                AttributeName=Artist,KeyType=HASH \
+                AttributeName=SongTitle,KeyType=RANGE \
+        --provisioned-throughput \
+                ReadCapacityUnits=10,WriteCapacityUnits=5
+
+echo "########### Inserting test data into a table ###########"
+aws --endpoint-url=http://localhost:4566 dynamodb put-item \
+    --table-name Music  \
+    --item \
+        '{"Artist": {"S": "No One You Know"}, "SongTitle": {"S": "Call Me Today"}, "AlbumTitle": {"S": "Somewhat Famous"}, "Awards": {"N": "1"}}'
+
+echo "########### Describing a status ###########"
+aws --endpoint-url=http://localhost:4566 dynamodb describe-table --table-name Music | grep TableStatus
