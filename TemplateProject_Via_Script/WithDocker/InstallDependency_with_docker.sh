4b945085c3411d6ee87deb7d6be6d1303f8e645a
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2022-05-14 21:09:47 -0300

    Este commit atualiza os arquivos de lauch.json

--- TemplateProject_Via_Script/WithDocker/InstallDependency_with_docker.sh
+++ TemplateProject_Via_Script/WithDocker/InstallDependency_with_docker.sh
@@ -49,6 +49,7 @@ module.exports = app;" > config/server.js
 mkdir routes
 touch routes/routes.js
 echo "module.exports = function(app){
+    // #swagger.parameters['id'] = { description: 'ID do usuário.' }
     app.get('/' ,function(req , res){
         const serviceExample = app.services.servicesSample; 
         res.send(serviceExample)
