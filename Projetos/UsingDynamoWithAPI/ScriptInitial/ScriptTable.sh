e1c07c06a4f8f43e7ddc30f2f47cc9981bc67a68
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2022-11-12 16:27:22 -0300

    Este commit aplicado cria o count com A e B

--- Projetos/UsingDynamoWithAPI/ScriptInitial/ScriptTable.sh
+++ Projetos/UsingDynamoWithAPI/ScriptInitial/ScriptTable.sh
@@ -16,7 +16,26 @@ echo "########### Inserting test data into a table ###########"
 aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     --table-name ExampleTable  \
     --item \
-        '{ "campo1Org": { "S": "xmujr7ryw#pfvwljuku#mha3ott6g" }, "campo2Unid": { "S": "00fld7j7l" }, "campo3Amigo": { "S": "5pir44f5c" }, "campo4Hash": { "S": "rd596ctsk" }, "campo5cod_text": { "S": "zpmmya9b" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
+        '{ "campo1Org": { "S": "xmujr7ryw" }, "campo2Unid": { "S": "00fld7j7l" }, "campo3Amigo": { "S": "5pir44f5c" }, "campo4Hash": { "S": "rd596ctsk" }, "campo5cod_text": { "S": "zpmmya9b" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
+
+aws --endpoint-url=http://localhost:4566 dynamodb put-item \
+    --table-name ExampleTable  \
+    --item \
+        '{ "campo1Org": { "S": "xmujr7ryw" }, "campo2Unid": { "S": "mprlxg6r" }, "campo3Amigo": { "S": "5pir44f5c" }, "campo4Hash": { "S": "lbcdq0qoz" }, "campo5cod_text": { "S": "tav9ji5or" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
+
+
+aws --endpoint-url=http://localhost:4566 dynamodb put-item \
+    --table-name ExampleTable  \
+    --item \
+        '{ "campo1Org": { "S": "xmujr7ryw" }, "campo2Unid": { "S": "00fld7j7l" }, "campo3Amigo": { "S": "6uafwkkw8" }, "campo4Hash": { "S": "rxvx7hui6" }, "campo5cod_text": { "S": "bpwojljcd" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
+
+
+
+aws --endpoint-url=http://localhost:4566 dynamodb put-item \
+    --table-name ExampleTable  \
+    --item \
+        '{ "campo1Org": { "S": "qavsrds2" }, "campo2Unid": { "S": "jbggjrapn" }, "campo3Amigo": { "S": "vppv7cp5v" }, "campo4Hash": { "S": "0ovwx0sdd" }, "campo5cod_text": { "S": "oimstnti8" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
+
 
 echo "########### Describing a status ###########"
 aws --endpoint-url=http://localhost:4566 dynamodb describe-table --table-name ExampleTable | grep TableStatus
--- Projetos/UsingDynamoWithAPI/services/servicesSample.js
+++ Projetos/UsingDynamoWithAPI/services/servicesSample.js
@@ -1,3 +1,29 @@
 module.exports = async function (app) {
-    return await app.repository.repositorySample.callDynamoDBGetAllItems();
+    let object = await app.repository.repositorySample.callDynamoDBGetAllItems();
+
+    console.log(object.Items)
+
+    let objectjson = {};
+
+    let i = 0;
+    let j = 0;
+    const objectFinal = object.Items.map(objItem => {
+        switch (objItem["campo3Amigo"]) {
+            case "5pir44f5c":
+                i++;
+                break;
+
+            default:
+                j++;
+                break;
+        }
+
+    });
+
+    objectjson.total = i + j;
+    objectjson.totalA = j;
+    objectjson.totalB = i;
+
+    console.log(`count total amigo: ${i}, \n count total sem amigo: ${j}`)
+    return objectjson;
 }
