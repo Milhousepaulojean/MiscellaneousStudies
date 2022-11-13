19b2592412857a86d4a93018345e3da088225c91
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2022-11-13 08:46:45 -0300

    Este commit ajusta o script da tabela pra retirada de atributos e mudana no attributeName

--- Projetos/UsingDynamoWithAPI/ScriptInitial/ScriptTable.sh
+++ Projetos/UsingDynamoWithAPI/ScriptInitial/ScriptTable.sh
@@ -4,11 +4,11 @@ echo "########### Creating table with global secondary index ###########"
         aws dynamodb --endpoint-url=http://localhost:4566 create-table \
             --table-name ExampleTable \
             --attribute-definitions \
-                AttributeName=campo1Org,AttributeType=S \
-                AttributeName=campo2Unid,AttributeType=S \
+                AttributeName=campo01Obj,AttributeType=S \
+                AttributeName=campo2Hash,AttributeType=S \
             --key-schema \
-                AttributeName=campo1Org,KeyType=HASH \
-                AttributeName=campo2Unid,KeyType=RANGE \
+                AttributeName=campo01Obj,KeyType=HASH \
+                AttributeName=campo2Hash,KeyType=RANGE \
         --provisioned-throughput \
                 ReadCapacityUnits=10,WriteCapacityUnits=5
 
@@ -16,25 +16,25 @@ echo "########### Inserting test data into a table ###########"
 aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     --table-name ExampleTable  \
     --item \
-        '{ "campo1Org": { "S": "xmujr7ryw" }, "campo2Unid": { "S": "00fld7j7l" }, "campo3Amigo": { "S": "5pir44f5c" }, "campo4Hash": { "S": "rd596ctsk" }, "campo5cod_text": { "S": "zpmmya9b" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
+        '{ "campo01Obj": { "S": "xmujr7ryw#00fld7j7l#5pir44f5c" }, "campo2Hash": { "S": "rd596ctsk" }, "campo3cod_text": { "S": "zpmmya9b" }, "campo4tip": { "S": "people" },  "campo5nom": { "S": "Fulano de tal" }, "createdate":{"S":"2022-10-21T17:42:34+00:00"} }'
 
 aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     --table-name ExampleTable  \
     --item \
-        '{ "campo1Org": { "S": "xmujr7ryw" }, "campo2Unid": { "S": "mprlxg6r" }, "campo3Amigo": { "S": "5pir44f5c" }, "campo4Hash": { "S": "lbcdq0qoz" }, "campo5cod_text": { "S": "tav9ji5or" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
-
-
-aws --endpoint-url=http://localhost:4566 dynamodb put-item \
-    --table-name ExampleTable  \
-    --item \
-        '{ "campo1Org": { "S": "xmujr7ryw" }, "campo2Unid": { "S": "00fld7j7l" }, "campo3Amigo": { "S": "6uafwkkw8" }, "campo4Hash": { "S": "rxvx7hui6" }, "campo5cod_text": { "S": "bpwojljcd" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
-
+        '{ "campo01Obj": { "S": "xmujr7ryw#mprlxg6r#5pir44f5c" }, "campo2Hash": { "S": "lbcdq0qoz" }, "campo3cod_text": { "S": "tav9ji5or" }, "campo4tip": { "S": "people" }, "campo5nom": { "S": "Fulano de tal" }, "createdate":{"S":"2022-10-21T17:42:35+00:00"} }'
 
 
 aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     --table-name ExampleTable  \
     --item \
-        '{ "campo1Org": { "S": "qavsrds2" }, "campo2Unid": { "S": "jbggjrapn" }, "campo3Amigo": { "S": "vppv7cp5v" }, "campo4Hash": { "S": "0ovwx0sdd" }, "campo5cod_text": { "S": "oimstnti8" }, "campo6tip": { "S": "people" }, "campo7ori": { "S": "m" }, "campo8nom": { "S": "Fulano de tal" } }'
+        '{ "campo01Obj": { "S": "xmujr7ryw#00fld7j7l#6uafwkkw8" }, "campo2Hash": { "S": "rxvx7hui6" }, "campo3cod_text": { "S": "bpwojljcd" }, "campo4tip": { "S": "people" }, "campo5nom": { "S": "Fulano de tal" }, "createdate":{"S":"2022-10-21T17:42:36+00:00"} }'
+
+
+
+aws --endpoint-url=http://localhost:4566 dynamodb put-item \
+    --table-name ExampleTable  \
+    --item \
+        '{ "campo01Obj": { "S": "qavsrds2#jbggjrapn#vppv7cp5v" }, "campo2Hash": { "S": "0ovwx0sdd" }, "campo3cod_text": { "S": "oimstnti8" }, "campo4tip": { "S": "people" },  "campo5nom": { "S": "Fulano de tal" }, "createdate":{"S":"2022-10-21T17:42:37+00:00"} }'
 
 
 echo "########### Describing a status ###########"
