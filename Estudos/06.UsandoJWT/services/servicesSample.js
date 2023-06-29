2c9e2da0187690463591af74b12c47d015e4573a
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2023-06-28 21:42:43 -0300

    Primeiro commit

--- Estudos/06.UsandoJWT/services/servicesSample.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-    //return app.repository.repositorySample;
-    this.callServicesPost = async function (params) {
-        return params;
-    }
-
-    return this;
-}
