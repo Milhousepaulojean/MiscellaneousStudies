0d93f6f1a3786f011f99ea9deb9ea22d322c7d58
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2023-10-12 20:40:30 -0300

    Este commit tem como objetivo adicionar de forma simples um script inicial

---
+++ docker/MongoDB/docker-entrypoint-initdb.d/mongo-init.js
@@ -0,0 +1,34 @@
+db.createUser(
+    {
+        user: "admin",
+        pwd: "password",
+        roles: [
+            {
+                role: "readWrite",
+                db: "sample_db"
+            }
+        ]
+    }
+);
+
+db = db.getSiblingDB('sample_db');
+
+db.createCollection('sample_collection');
+
+db.sample_collection.insertMany([
+    {
+        org: 'helpdev',
+        filter: 'EVENT_A',
+        addrs: 'http://rest_client_1:8080/wh'
+    },
+    {
+        org: 'helpdev',
+        filter: 'EVENT_B',
+        addrs: 'http://rest_client_2:8081/wh'
+    },
+    {
+        org: 'github',
+        filter: 'EVENT_C',
+        addrs: 'http://rest_client_3:8082/wh'
+    }
+]);
